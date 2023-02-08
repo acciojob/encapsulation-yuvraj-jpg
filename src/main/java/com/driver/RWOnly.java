0b9daa54fd1c1c64a8d9a60d040496ba742655e0
Yuvraj <yv96875@gmail.com>
2023-02-08 22:00:29 +0530

    Encapsulation

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
